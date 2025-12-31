aa4eb928b8bdbf0c21160b7105cfcab75edbf738
Paweł Sawicki <63120250+Reacher2020@users.noreply.github.com>
2025-12-31 10:26:48 +0100

    qqqaaa

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./coach/Index";
+import Index from "./pages/coach/Index";
 import Clients from "./pages/coach/Clients";
 import CalendarPage from "./pages/coach/Calendar";
 import Workouts from "./pages/coach/Workouts";
