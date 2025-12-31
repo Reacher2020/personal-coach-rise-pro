f650a90bb34f0a46ffe2ae51536d6ab0e5fd85a7
Paweł Sawicki <63120250+Reacher2020@users.noreply.github.com>
2025-12-31 10:52:08 +0100

    axsz

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/coach/Index";
+import Index from "./pages/Index";
 import Clients from "./pages/Clients";
 import CalendarPage from "./pages/Calendar";
 import Workouts from "./pages/Workouts";
@@ -22,7 +22,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/coach/" element={<Index />} />
+          <Route path="/" element={<Index />} />
           <Route path="/clients" element={<Clients />} />
           <Route path="/calendar" element={<CalendarPage />} />
           <Route path="/workouts" element={<Workouts />} />
